c83bb4acfe1aa2262e408ad32f6e304c62994db0
Vivek Kumar Singh <Vivs1206@gmail.com>
2017-08-29 14:28:47 +0530

    ADD AL

---
+++ 1RDLC/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using _1RDLC.Models;
+
+namespace _1RDLC.Controllers
+{
+    public class HomeController : Controller
+    {
+        SkillDbEntities sdEnt = new SkillDbEntities();
+        // GET: Home
+        public ActionResult Index()
+        {
+            List<tblEmp> tblEmpList = sdEnt.tblEmps.ToList();
+            return View(tblEmpList);
+        }
+
+        public ActionResult Report()
+        {
+            return Redirect("~/Report/WebForm1.aspx");
+        }
+    }
+}
---
+++ 1RDLC/Report/WebForm1.aspx.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Reporting.WebForms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using _1RDLC.Models;
+
+namespace _1RDLC.Report
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                SkillDbEntities SDEnt = new SkillDbEntities();            
+                List<tblEmp> Emp = SDEnt.tblEmps.ToList();
+                //ReportViewer1.LocalReport.ReportPath = Server.MapPath("~/Report/Report1.rdlc");
+                ReportViewer2.LocalReport.DataSources.Clear();
+                ReportDataSource RDS = new ReportDataSource("DataSet1", Emp);
+                ReportViewer2.LocalReport.DataSources.Add(RDS);
+                ReportViewer2.LocalReport.Refresh();
+            }
+
+        }
+    }
+}
